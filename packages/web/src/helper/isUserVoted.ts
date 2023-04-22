5cfbcfc003c80d99a722280480b0e0a3f106b35f
CoolColdUK <coolcold@gmail.com>
2023-04-22 20:13:43 +0100

    undefined means not voted

--- packages/web/src/helper/isUserVoted.ts
+++ packages/web/src/helper/isUserVoted.ts
@@ -2,5 +2,5 @@ import {VoteEnum} from '../enum/VoteEnum';
 import {VoteUser} from '../interface/VoteUser';
 
 export default function isUserVoted(user: VoteUser): boolean {
-  return user.vote !== VoteEnum.NOT_VOTED;
+  return !(user.vote === VoteEnum.NOT_VOTED || user.vote === undefined);
 }
