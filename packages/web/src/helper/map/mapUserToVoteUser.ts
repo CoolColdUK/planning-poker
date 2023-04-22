1cdccd9bc507d8471ca98f0c7b62c4ff6294e6e9
CoolColdUK <coolcold@gmail.com>
2023-04-22 19:50:34 +0100

    move map file

--- packages/web/src/helper/map/mapUserToVoteUser.ts
+++ packages/web/src/helper/map/mapUserToVoteUser.ts
@@ -1,6 +1,6 @@
 import {User} from 'firebase/auth';
-import {VoteUser} from '../interface/VoteUser'; // Import the VoteUser interface from the separate file you created
-import {VoteEnum} from '../enum/VoteEnum';
+import {VoteUser} from '../../interface/VoteUser'; // Import the VoteUser interface from the separate file you created
+import {VoteEnum} from '../../enum/VoteEnum';
 
 export default function mapUserToVoteUser(user: User, vote: VoteEnum = VoteEnum.NOT_VOTED): VoteUser {
   const {uid, displayName} = user;
