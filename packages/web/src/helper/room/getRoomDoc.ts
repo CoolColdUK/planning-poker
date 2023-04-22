d8e8536c3c1415e100f8793024da088bd58f5e10
CoolColdUK <coolcold@gmail.com>
2023-04-22 19:57:54 +0100

    add data type in doc ref

--- packages/web/src/helper/room/getRoomDoc.ts
+++ packages/web/src/helper/room/getRoomDoc.ts
@@ -1,6 +1,7 @@
-import {doc} from 'firebase/firestore';
+import {DocumentReference, doc} from 'firebase/firestore';
 import {firestore} from '../../firebaseConfig';
+import {RoomData} from '../../interface/RoomData';
 
 export default function getRoomDoc(roomId: string) {
-  return doc(firestore, 'rooms', roomId);
+  return doc(firestore, 'rooms', roomId) as DocumentReference<RoomData>;
 }
