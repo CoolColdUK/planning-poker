21beda93ecc9d2870d0b6c70563194f8316881b2
CoolColdUK <coolcold@gmail.com>
2023-04-22 19:56:02 +0100

    recreate room after 1 day

--- packages/web/src/hook/useCheckAndCreateRoom.ts
+++ packages/web/src/hook/useCheckAndCreateRoom.ts
@@ -2,21 +2,34 @@ import {User} from 'firebase/auth';
 import {useEffect, useState} from 'react';
 import createRoom from '../helper/room/createRoom';
 import isRoomExists from '../helper/room/isRoomExists';
+import {getDoc} from 'firebase/firestore';
+import getRoomDoc from '../helper/room/getRoomDoc';
+import dayjs from 'dayjs';
 
 export const useCheckAndCreateRoom = (user: User, roomId: string) => {
   const [currentRoomId, setCurrentRoomId] = useState<string | null>(null);
-  console.log('useCheckAndCreateRoom', roomId);
+  // console.log('useCheckAndCreateRoom', roomId);
 
   useEffect(() => {
     if (!roomId) return;
-    console.log('join');
+    // console.log('join');
 
     async function join() {
       const roomExists = await isRoomExists(roomId);
 
       if (!roomExists) {
-        console.log('create');
+        // console.log('create');
         await createRoom(roomId, user);
+      }
+
+      //reload room if opened for too long
+      const doc = await getDoc(getRoomDoc(roomId));
+      const data = doc.data();
+      if (data) {
+        const createdDate = dayjs(data.createdAt.toDate());
+        if (createdDate.isBefore(dayjs().subtract(1, 'day'))) {
+          await createRoom(roomId, user);
+        }
       }
 
       setCurrentRoomId(roomId);
